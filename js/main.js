033f7b0537b121b78c694930609afe4bc54ce197
wagngomes <wagner.gomes.cx2@gmail.com>
2023-03-11 12:16:21 -0300

    versão final

--- js/main.js
+++ js/main.js
@@ -25,10 +25,10 @@ form.addEventListener("submit", (evento) => {
 
         itemAtual.id = existe.id
         atualizaElemento(itemAtual)
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itensAtual
     }else{
 
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1]).id +1: 0;
 
         criaElemento(itemAtual)
 
@@ -54,6 +54,8 @@ function criaElemento(item) {
 
     novoItem.innerHTML += item.nome
 
+    novoItem.appendChild(botaoDeleta(item.id))
+
     lista.appendChild(novoItem)
 
 }
@@ -61,4 +63,25 @@ function criaElemento(item) {
 function atualizaElemento (item) {
 
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "x"
+
+    elementoBotao.addEventListener("click", function() {
+
+        deletaElemento(this.parentNode, id)
+
+    })
+    return elementoBotao
+
+}
+
+function deletaElemento(tag, id) {
+
+    tag.remove()
+    itens.splice(itens.findIndex(elemento => elemento.id == id), 1)
+    localStorage.setItem("itens", JSON.stringify(itens))
+
 }
